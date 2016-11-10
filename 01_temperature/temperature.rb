3b7024a3bb7d7a4281069f30b9030bf7dde1d15a
J4K0B <jakob.tepe@gmail.com>
2016-11-10 18:59:40 +0100

    lessons until 02 finished

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc fah
+	c = (fah - 32) / 1.8
+	c.round
+end
+def ctof cel
+	f = cel * 1.8 + 32
+	
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add a, b 
+	a + b 
+end
+def subtract a, b 
+	a - b 
+end
+def sum array
+	summe = 0
+	array.each do |a|
+		summe = summe + a
+	end
+	summe
+end
+def multiply a, b
+	a * b 
+end
+def power a, b 
+	a ** b 
+end
+def factorial a
+	if a <= 1
+		fac = 1
+	else 
+		fac = a* factorial(a-1)
+	end
+	fac
+end
